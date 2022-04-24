4d73afd07bf1f79fd4d0812483951e415e833f4f
Thule42 <104212853+Thule42@users.noreply.github.com>
2022-04-24 15:58:05 +0700

    first commit

---
+++ FoodCompany.sql
@@ -0,0 +1,86 @@
+﻿CREATE DATABASE FoodCompany
+GO
+
+USE FoodCompany
+GO
+
+CREATE TABLE UserLogin(
+	UserName nvarchar(20),
+	UserPassword nvarchar(20),
+	PRIMARY KEY(UserName)
+)
+GO
+
+CREATE TABLE Employee(
+	EmployeeID nvarchar(50),
+	EmployeeName nvarchar(50),
+	Gender varchar(5),
+	Birthdate datetime,
+	Phone nvarchar(50),
+	Email nvarchar(50),
+	EmployeeAdress nvarchar(50),
+	CMNN nvarchar(50)
+	PRIMARY KEY(EmployeeID)
+)
+GO
+
+CREATE TABLE Product(
+	ProductID nvarchar(50),
+	ProductName nvarchar(50),
+	Quantity int,
+	Price decimal(18,0),
+	PRIMARY KEY(ProductID)
+)
+GO
+
+CREATE TABLE Agent(
+	AgentID nvarchar(50),
+	AgentName nvarchar(50),
+	AgentAdress nvarchar(50),
+	Phone nvarchar(50)
+	PRIMARY KEY(AgentID)
+)
+
+CREATE TABLE Import(
+	ImportID nvarchar(50),
+	ProductID nvarchar(50) FOREIGN KEY REFERENCES Product(ProductID),
+	ProductName nvarchar(50),
+	ImportDate date,
+	Quantity int,
+	Price decimal(18,0),
+
+	PRIMARY KEY(ImportID)
+)
+GO
+
+CREATE TABLE Export(
+	ExportID nvarchar(50),
+	ProductID nvarchar(50) FOREIGN KEY REFERENCES Product(ProductID),
+	ProductName nvarchar(50),
+	AgentID nvarchar(50) FOREIGN KEY REFERENCES Agent(AgentID),
+	ExportDate date,
+	Quantity int,
+	Price decimal(18,0),
+	DeliveryStatus nvarchar(50),
+	PaymentStatus nvarchar(50),
+	PRIMARY KEY(ExportID)
+)
+GO
+
+CREATE TABLE RevenueMonth(
+	RevenueID int identity,
+	Months int,
+	Revenue decimal(18,0)
+	PRIMARY KEY(RevenueID)
+)
+GO
+
+
+
+INSERT INTO UserLogin VALUES('Thu', '123456')
+INSERT INTO UserLogin VALUES('Thuan', '123456')
+INSERT INTO UserLogin VALUES('Vy','123456')
+SELECT * FROM UserLogin
+
+INSERT INTO Employee VALUES(N'001',N'Đoàn Chính Thuần',N'Nam',N'09823144534',CAST(N'2001-1-30' AS Date), N'doanchinhthuan@gmail.com',N'22 CMT8 Q,Tân Bình TpHCM',454674289)
+SELECT * FROM Employee
---
+++ SalesManagement/SalesManagement/Employee.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace SalesManagement
+{
+    partial class Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmployID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmploName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEmployAdress = new System.Windows.Forms.TextBox();
+            this.txtCMNN = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtEmployID
+            // 
+            this.txtEmployID.Location = new System.Drawing.Point(213, 29);
+            this.txtEmployID.Name = "txtEmployID";
+            this.txtEmployID.Size = new System.Drawing.Size(244, 22);
+            this.txtEmployID.TabIndex = 0;
+            this.txtEmployID.TextChanged += new System.EventHandler(this.txtEmployID_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // txtEmploName
+            // 
+            this.txtEmploName.Location = new System.Drawing.Point(213, 68);
+            this.txtEmploName.Name = "txtEmploName";
+            this.txtEmploName.Size = new System.Drawing.Size(244, 22);
+            this.txtEmploName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(561, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(561, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Phone";
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(671, 78);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(244, 22);
+            this.txtBirthDate.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(424, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Employee Profile";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.txtGender);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtCMNN);
+            this.groupBox1.Controls.Add(this.txtEmployAdress);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtBirthDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtEmploName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtEmployID);
+            this.groupBox1.Location = new System.Drawing.Point(30, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1056, 215);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Address";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(561, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 31);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(36, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 25);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "CMNN";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(561, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 25);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Birth Date";
+            // 
+            // txtEmployAdress
+            // 
+            this.txtEmployAdress.Location = new System.Drawing.Point(213, 111);
+            this.txtEmployAdress.Name = "txtEmployAdress";
+            this.txtEmployAdress.Size = new System.Drawing.Size(244, 22);
+            this.txtEmployAdress.TabIndex = 12;
+            // 
+            // txtCMNN
+            // 
+            this.txtCMNN.Location = new System.Drawing.Point(213, 160);
+            this.txtCMNN.Name = "txtCMNN";
+            this.txtCMNN.Size = new System.Drawing.Size(244, 22);
+            this.txtCMNN.TabIndex = 13;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(671, 119);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(244, 22);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(671, 156);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(244, 22);
+            this.txtPhone.TabIndex = 15;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(671, 26);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(244, 22);
+            this.txtGender.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1101, 338);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 672);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Name = "Employee";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Employee_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmployID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmploName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCMNN;
+        private System.Windows.Forms.TextBox txtEmployAdress;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
